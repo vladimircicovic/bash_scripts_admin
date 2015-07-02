1fbeabed59b78810da61bfd57469ad0537569631
Vladimir Cicovic <vladimirc1981@gmail.com>
2015-07-02 01:47:41 +0000

    caesar.sh changes

--- caesar.sh
+++ caesar.sh
@@ -4,5 +4,7 @@ echo "Input some sentence:"
 
 read input
 
-echo crypted:
+echo Crypted/De-crypted:
 echo $input | tr abcdefghijklmnopqrstuvwxyz qwertyuiopasdfghjklzxcvbnm 
+
+echo To get decrypted use same crypted output for input of this script 
