80ce21cde6327f1bb1b51cc4e6fa87c51d732dc6
Vladimir Cicovic <vladimirc1981@gmail.com>
2015-07-02 01:12:59 +0000

    Add new lines to netcheck

--- netcheck.sh
+++ netcheck.sh
@@ -1,6 +1,7 @@
+( curl -version > /dev/null 2>&1 ) ||  ( echo no curl installed; sleep 100000)
 SLEEP_TIME=2
 SITE=gmail.com
 while true; do
-(curl -s $SITE > /dev/null 2>&1)|| (echo no)
+(curl -s $SITE > /dev/null 2>&1) || (echo no net)
 sleep $SLEEP_TIME
 done
