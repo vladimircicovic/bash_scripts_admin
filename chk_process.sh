460b6184d076c8f89e468583637db52106ba7dd8
Vladimir Cicovic <vladimirc1981@gmail.com>
2015-07-11 04:53:53 +0000

    netcheck solve issue

--- chk_process.sh
+++ chk_process.sh
@@ -2,7 +2,7 @@
 #Best would be usage trough crontab with 1 min check - if you need check for ex each 2 sec - send secret email vladimirc1981 A gmail d0t com
 #This script is monitoring process if goes off - it start again
 
-(pidof pidofz > /dev/null 2>&1)||(echo no pidof tool ) && exit
+(pidof pidofz > /dev/null 2&>1)||(echo no pidof tool ) && exit
 
 
 PID=$(pidof $1)
